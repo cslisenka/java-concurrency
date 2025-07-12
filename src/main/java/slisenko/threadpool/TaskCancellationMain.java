b96b4b2c4ba2d4cf15fa8a9b849dc2dac169ae8c
Kanstantsin Slisenka <kslisenko@MacBook-Air-Kate.local>
2025-07-12 15:09:03 +0200

    Task cancellation example

--- src/main/java/slisenko/threadpool/TaskCancellationMain.java
+++ src/main/java/slisenko/threadpool/TaskCancellationMain.java
@@ -1,16 +1,44 @@
 package slisenko.threadpool;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import static slisenko.util.MyLogger.log;
+import static slisenko.util.ThreadUtil.sleep;
+
 public class TaskCancellationMain {
 
+    // TODO example for handling the thread interrupt
     public static void main(String[] args) {
-        // TODO create thread pool
+        ExecutorService pool = Executors.newSingleThreadExecutor();
 
-        // TODO create tasks
+        Runnable task1 = () -> {
+            log("Start task 1");
+            sleep(15_000);
+            log("Finish 1");
+        };
 
-        // TODO cancel tasks and see if it is done
+        Runnable task2 = () -> {
+            log("Start task 2");
+            sleep(15_000);
+            log("Finish 2");
+        };
 
-        // TODO shutdown pool
+        Future<?> future1 = pool.submit(task1);
+        Future<?> future2 = pool.submit(task2);
 
+        log("Task 1 status %s", future1.state());
+        log("Task 2 status %s", future2.state());
 
+        // This task is running and will be completed unless we interrupt
+        boolean cancellation1 = future1.cancel(true); // This task is only interrupted, so sleep method throws interrupted exception
+        boolean cancellation2 = future2.cancel(true); // This task wasn't started, so cancelled
+        log("Cancelling both tasks (%b, %b)", cancellation1, cancellation2);
+
+        log("Task 1 status %s, cancelled=%b", future1.state(), future1.isCancelled());
+        log("Task 2 status %s, cancelled=%b", future2.state(), future2.isCancelled());
+
+        pool.shutdown();
     }
 }
