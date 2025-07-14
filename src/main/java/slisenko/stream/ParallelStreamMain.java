f1bb2c8ea45887e09c0bb73eee9207d1a677d02f
Kanstantsin Slisenka <kslisenko@MacBook-Air-Kate.local>
2025-07-14 16:59:08 +0200

    Add parallel stream example

--- src/main/java/slisenko/stream/ParallelStreamMain.java
+++ src/main/java/slisenko/stream/ParallelStreamMain.java
@@ -2,6 +2,9 @@ package slisenko.stream;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static slisenko.util.MyLogger.log;
 import static slisenko.util.ThreadUtil.sleep;
@@ -9,6 +12,15 @@ import static slisenko.util.ThreadUtil.sleep;
 public class ParallelStreamMain {
 
     public static void main(String[] args) throws InterruptedException {
+        List<Integer> numbers = IntStream.range(1, 1000).boxed().toList();
 
+        // Using all common pool + main as there are no empty threads
+        numbers
+            .stream()
+            .parallel()
+            .forEach(n -> {
+                log("n=%d", n);
+                sleep(1000);
+            });
     }
 }
